fdbb330d6799209801c791fdf4a36fb626b7eca7
mohammad <mfeili@gmail.com>
2019-10-24 15:07:21 +0330

    Added viewset to our arofiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework import status
 from profiles_api import serializers
+
 
 
 class HelloApiView(APIView):
@@ -37,3 +39,50 @@ class HelloApiView(APIView):
 		return Response({'method':'PATCH'})
 	def delete(self, request, pk=None):
 		return Response({'method':'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+	"""Test API Viewset"""
+
+	serializer_class = serializers.HelloSerializer
+	def list(self, request):
+		""" Return a hello message """
+
+		a_viewset=[
+		'Uses actions (list, create, retrieve, update, partial_update)',
+		'Automatically maps to URLs using Routers',
+		'Provide more functionality with less code'
+		]
+
+		return Response({'message':"Hello", 'a_viewset':a_viewset})
+
+	def create(self, request):
+		""" Create a new hello message """
+
+		serializer = self.serializer_class(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f"Hello {name}!"
+			return Response({'message': message})
+
+		else:
+			return Response(
+			serializer.errors,
+			status.HTTP_400_BAD_REQUEST
+			)
+
+	def retrieve(self, request, pk=None):
+		""" Handle getting an object by its ID """
+		return Response({"http_method": "retrieve"})
+
+	def update(self, request, pk=None):
+		""" Handle updating an object by its ID """
+		return Response({"http_method": "update"})
+
+	def partial_update(self, request, pk=None):
+		""" Handle updating the part of object """
+		return Response({"http_method": "partial_update"})
+
+	def destroy(self, request, pk=None):
+		""" Handle removing an object """
+		return Response({"http_mothod": "destroy"})
